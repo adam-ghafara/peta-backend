3099849a326feb79b652c4e511e08ea5f66da8eb
adam-ghafara <adam.ghafara.job@gmail.com>
2023-10-23 15:17:25 +0700

    perbaikan Peda

--- peda.go
+++ peda.go
@@ -8,16 +8,16 @@ import (
 	"github.com/whatsauth/watoken"
 )
 
-func GCFHandler(MONGOCONNSTRINGENV, dbname, collectionname string) string {
-	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+func GCFHandler(MONGODATA, dbname, collectionname string) string {
+	mconn := SetConnection(MONGODATA, dbname)
 	datagedung := GetAllBangunanLineString(mconn, collectionname)
 	return GCFReturnStruct(datagedung)
 }
 
-func GCFPostHandler(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+func GCFPostHandler(PASETOPRIV, MONGODATA, dbname, collectionname string, r *http.Request) string {
 	var Response Credential
 	Response.Status = false
-	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	mconn := SetConnection(MONGODATA, dbname)
 	var datauser User
 	err := json.NewDecoder(r.Body).Decode(&datauser)
 	if err != nil {
@@ -25,7 +25,7 @@ func GCFPostHandler(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionn
 	} else {
 		if IsPasswordValid(mconn, collectionname, datauser) {
 			Response.Status = true
-			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIV))
 			if err != nil {
 				Response.Message = "Token Encode Fail : " + err.Error()
 			} else {
